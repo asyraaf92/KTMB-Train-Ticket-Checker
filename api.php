e702b9b2d0d76131f9f70092d9ec07a5cee130d6
afzafri <zafri_21596@yahoo.com>
2017-03-27 18:48:46 +0800

    done get train list

--- api.php
+++ api.php
@@ -27,7 +27,15 @@ if($option == 'GetDest')
 
 if($option == 'GetTrain')
 {
+	$originCode = $_GET['Origin'];
+	$destCode = $_GET['Destination'];
+	$date = $_GET['Date'];
 
+	$url = "https://intranet.ktmb.com.my/e-ticket/Ajax/GetTrainList.aspx?Origin=".$originCode."&Destination=".$destCode."&Tarikh=".$date;
+
+    $result = file_get_contents($url);
+
+    echo $result;
 }
 
 if($option == 'GetCoach')

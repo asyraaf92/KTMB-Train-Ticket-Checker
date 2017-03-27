e393b451da3f7d702779bda0a63b39071adbdb46
afzafri <zafri_21596@yahoo.com>
2017-03-27 18:23:37 +0800

    started API

---
+++ api.php
@@ -0,0 +1,30 @@
+<?php
+/*
+	Codes for fetching data from KTMB E-Ticket Page
+*/
+
+$option = (isset($_GET['option'])) ? $_GET['option'] : '';
+
+if($option == 'GetOrigin')
+{
+
+}
+
+if($option == 'GetDest')
+{
+
+}
+
+if($option == 'GetTrain')
+{
+
+}
+
+if($option == 'GetCoach')
+{
+	
+}
+
+
+?>
+

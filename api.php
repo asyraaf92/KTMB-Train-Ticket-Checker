1c92b542b68f6fe59655d2f97f34c450a270f4c2
afzafri <zafri_21596@yahoo.com>
2017-03-27 18:40:06 +0800

    done get origin and destination states

--- api.php
+++ api.php
@@ -7,12 +7,22 @@ $option = (isset($_GET['option'])) ? $_GET['option'] : '';
 
 if($option == 'GetOrigin')
 {
+	$url = "https://intranet.ktmb.com.my/e-ticket/Ajax/GetOriginDest.aspx";
 
+    $result = file_get_contents($url);
+
+    echo $result;
 }
 
 if($option == 'GetDest')
 {
+	$originCode = $_GET['origin'];
 
+	$url = "https://intranet.ktmb.com.my/e-ticket/Ajax/GetOriginDest.aspx?Origin=".$originCode;
+
+    $result = file_get_contents($url);
+
+    echo $result;
 }
 
 if($option == 'GetTrain')
@@ -22,7 +32,7 @@ if($option == 'GetTrain')
 
 if($option == 'GetCoach')
 {
-	
+
 }
 
 

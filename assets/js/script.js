cca7b86bbeb2bbf508b35f751265f15b592f9226
afzafri <zafri_21596@yahoo.com>
2017-03-29 18:45:45 +0800

    fetch and list Origins DONE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
+	// populate Origin select box
+	$.getJSON("./api.php?option=GetOrigin", function(data){
+		var states = "";
+        $.each(data, function(state, obj){
+        	var up = "<optgroup label='"+state+"'>";
+        	var options= "";
+
+        	for(var i=0;i<obj.length;i++)
+        	{
+        		options += "<option value='"+obj[i].Code+"'>"+obj[i].Nama+"</option>";
+        	}
+
+        	var down = "</optgroup>";
+
+        	states += up+options+down;
+            
+        });
+        $("#originSelect").append(states);
+    	$('#originSelect').material_select();
+    });
 
 	// initialize select box
     $('select').material_select();

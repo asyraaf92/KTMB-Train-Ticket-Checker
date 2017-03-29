28efb0f003d87efaa3d4feb3f742c6347a41fb77
afzafri <zafri_21596@yahoo.com>
2017-03-29 19:03:09 +0800

    Added loading spinner

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 
 	// populate Origin select box
 	$.getJSON("./api.php?option=GetOrigin", function(data){
+		// show loading
+		$('.se-pre-con').fadeIn('slow');
+
 		var states = "";
         $.each(data, function(state, obj){
         	var up = "<optgroup label='"+state+"'>";
@@ -17,8 +20,13 @@ $(document).ready(function() {
         	states += up+options+down;
             
         });
+
+        // append to select box
         $("#originSelect").append(states);
     	$('#originSelect').material_select();
+
+    	// hide loading
+		$('.se-pre-con').fadeOut('slow');
     });
 
 	// initialize select box

4807eb4cb352e816bbae51afb42154e6074455d1
afzafri <zafri_21596@yahoo.com>
2017-03-29 18:00:01 +0800

    fix datepicker always opening

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
 
+
 	// initialize select box
     $('select').material_select();
 
     // initialize datepicker
 	$('.datepicker').pickadate({
+		onClose: function(){
+	        $(document.activeElement).blur()
+	    },
 		selectMonths: true, // Creates a dropdown to control month
 		selectYears: 15, // Creates a dropdown of 15 years to control year
 		format: 'dd-mmm-yyyy',
 	});
+
+
 
 });
